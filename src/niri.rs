df169402d091b57436423d2f661410fb915b0753
Matthieu <matthieu.cx@gmail.com>
2025-07-24 21:57:05 +0200

    feat: 1 pixel wide black bar at the left of the screen (fix for my xps faulty screen)

--- src/niri.rs
+++ src/niri.rs
@@ -474,6 +474,8 @@ pub struct OutputState {
     screen_transition: Option<ScreenTransition>,
     /// Damage tracker used for the debug damage visualization.
     pub debug_damage_tracker: OutputDamageTracker,
+    /// Solid color buffer to always display a column of black pixels on the left of the screen
+    pub bar_buffer: SolidColorBuffer,
 }
 
 #[derive(Debug, Default)]
@@ -2925,6 +2927,7 @@ impl Niri {
             lock_color_buffer: SolidColorBuffer::new(size, CLEAR_COLOR_LOCKED),
             screen_transition: None,
             debug_damage_tracker: OutputDamageTracker::from_output(&output),
+            bar_buffer: SolidColorBuffer::new(Size::from((1, size.h)), Color32F::BLACK),
         };
         let rv = self.output_state.insert(output.clone(), state);
         assert!(rv.is_none(), "output was already tracked");
@@ -3029,6 +3032,7 @@ impl Niri {
         if let Some(state) = self.output_state.get_mut(output) {
             state.background_buffer.resize(output_size);
             state.backdrop_buffer.resize(output_size);
+            state.bar_buffer.resize(Size::from((1, output_size.h)));
 
             state.lock_color_buffer.resize(output_size);
             if let Some(lock_surface) = &state.lock_surface {
@@ -4110,11 +4114,23 @@ impl Niri {
 
         let output_scale = Scale::from(output.current_scale().fractional_scale());
 
+        let state = self.output_state.get(output).unwrap();
+        let bar = SolidColorRenderElement::from_buffer(
+            &state.bar_buffer,
+            (0, 0),
+            output_scale,
+            1.,
+            Kind::Unspecified,
+        )
+        .into();
+
         // The pointer goes on the top.
         let mut elements = vec![];
         if include_pointer {
             elements = self.pointer_element(renderer, output);
         }
+        // Then, the black bar
+        elements.insert(0, bar);
 
         // Next, the screen transition texture.
         {
